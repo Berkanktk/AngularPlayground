9ca1db8e6dabb26aa09bfbc1a3cd8f15e74dd7ca
Berkanktk <berkank@live.dk>
2023-02-17 22:11:36 +0100

    Added dashboard graphs

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,322 @@
+import { Component } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import * as echarts from 'echarts';
+
+function getVirtualData(year: string) {
+  const date = +echarts.time.parse(year + '-01-01');
+  const end = +echarts.time.parse(+year + 1 + '-01-01');
+  const dayTime = 3600 * 24 * 1000;
+  const data: [string, number][] = [];
+  for (let time = date; time < end; time += dayTime) {
+    data.push([
+      echarts.time.format(time, '{yyyy}-{MM}-{dd}', false),
+      Math.floor(Math.random() * 10000)
+    ]);
+  }
+  return data;
+}
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+
+  chartOption1: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    tooltip: {
+      trigger: 'item',
+      showDelay: 0,
+      transitionDuration: 0.2,
+      formatter: function (params: { [x: string]: any; }) {
+        return `<b>${params['name']}</b> : $ ${params['value']}`;
+      }
+    },
+    series: [{
+      data: [820, 932, 901, 934, 1290, 1430, 1550, 1200, 1650.1450, 1680, 1890, 2300],
+      type: 'line',
+      areaStyle: {},
+      smooth: true
+    }]
+  }
+
+  chartOption2: EChartsOption = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: 'Direct',
+        type: 'bar',
+        barWidth: '60%',
+        data: [10, 52, 200, 334, 390, 330, 220]
+      }
+    ]
+  };
+
+  chartOption3: EChartsOption = {
+    title: {
+      text: 'Referer of a Website',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  chartOption4: EChartsOption = {
+    title: {'text': 'Scatter Plot'},
+    xAxis: {'name': 'X'},
+    yAxis: {'name': 'Y'},
+    tooltip: {
+      formatter: params => {
+        return `(${"data" in params ? params.data : [0]})`;
+      }
+    },
+    series: [
+      {
+        symbolSize: 20,
+        data: [
+          [10.0, 8.04],
+          [8.07, 6.95],
+          [13.0, 7.58],
+          [9.05, 8.81],
+          [11.0, 8.33],
+          [14.0, 7.66],
+          [13.4, 6.81],
+          [10.0, 6.33],
+          [14.0, 8.96],
+          [12.5, 6.82],
+          [9.15, 7.2],
+          [11.5, 7.2],
+          [3.03, 4.23],
+          [12.2, 7.83],
+          [2.02, 4.47],
+          [1.05, 3.33],
+          [4.05, 4.96],
+          [6.03, 7.24],
+          [12.0, 6.26],
+          [12.0, 8.84],
+          [7.08, 5.82],
+          [5.02, 5.68]
+        ],
+        type: 'scatter'
+      }
+    ]
+  };
+
+  chartOption5: EChartsOption = {
+    title: {
+      text: 'Basic Radar Chart'
+    },
+    legend: {
+      data: ['Allocated Budget', 'Actual Spending']
+    },
+    radar: {
+      // shape: 'circle',
+      indicator: [
+        { name: 'Sales', max: 6500 },
+        { name: 'Administration', max: 16000 },
+        { name: 'Information Technology', max: 30000 },
+        { name: 'Customer Support', max: 38000 },
+        { name: 'Development', max: 52000 },
+        { name: 'Marketing', max: 25000 }
+      ]
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    series: [
+      {
+        name: 'Budget vs spending',
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 3000, 20000, 35000, 50000, 18000],
+            name: 'Allocated Budget'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: 'Actual Spending'
+          }
+        ]
+      }
+    ]
+  };
+
+  chartOption6: EChartsOption = {
+    series: [
+      {
+        type: 'treemap',
+        data: [
+          {
+            name: 'nodeA', // First tree
+            value: 10,
+            children: [
+              {
+                name: 'nodeAa', // First leaf of first tree
+                value: 4
+              },
+              {
+                name: 'nodeAb', // Second leaf of first tree
+                value: 6
+              }
+            ]
+          },
+          {
+            name: 'nodeB', // Second tree
+            value: 20,
+            children: [
+              {
+                name: 'nodeBa', // Son of first tree
+                value: 20,
+                children: [
+                  {
+                    name: 'nodeBa1', // Granson of first tree
+                    value: 20
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  chartOption7: EChartsOption = {
+    series: {
+      type: 'sunburst',
+      data: [
+        {
+          name: 'Grandpa',
+          children: [
+            {
+              name: 'Uncle Leo',
+              value: 15,
+              children: [
+                {
+                  name: 'Cousin Jack',
+                  value: 2
+                },
+                {
+                  name: 'Cousin Mary',
+                  value: 5,
+                  children: [
+                    {
+                      name: 'Jackson',
+                      value: 2
+                    }
+                  ]
+                },
+                {
+                  name: 'Cousin Ben',
+                  value: 4
+                }
+              ]
+            },
+            {
+              name: 'Father',
+              value: 10,
+              children: [
+                {
+                  name: 'Me',
+                  value: 5
+                },
+                {
+                  name: 'Brother Peter',
+                  value: 1
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'Nancy',
+          children: [
+            {
+              name: 'Uncle Nike',
+              children: [
+                {
+                  name: 'Cousin Betty',
+                  value: 1
+                },
+                {
+                  name: 'Cousin Jenny',
+                  value: 2
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      radius: [60, '90%'],
+      itemStyle: {
+        borderRadius: 7,
+        borderWidth: 2
+      },
+      label: {
+        show: true
+      }
+    }
+  };
+
+}
