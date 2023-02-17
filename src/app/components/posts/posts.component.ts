5839cbb61c6d4cdb94e418cd1081a003d6dd3ff2
Berkanktk <berkank@live.dk>
2023-02-17 16:42:23 +0100

    Refactored

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PostService } from '../services/post.service';
-import { Post } from '../Post';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../Post';
 
 
 @Component({
